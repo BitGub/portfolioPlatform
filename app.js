420a882336b08ff6fb974129166a28dbda811976
Shaun Gordon <shaundev.gordon@gmail.com>
2015-07-12 11:56:37 +0100

    routes config and views

--- app.js
+++ app.js
@@ -3,5 +3,5 @@
 var app = require('./lib/server');
 
 app.listen(app.get("port"), function(){
-  console.log('server is listening on port %d', 3000)
+  console.log('server is listening on port %d', app.get("port"))
 });
--- test.js
+++ test.js
@@ -15,7 +15,28 @@ describe('Requests Root path', function(){
 
       request(app)
         .get('/')
-        .expect('Content-Type', /html/, done);
+        .expect('Content-Type', /html/, done)
     });
     
+});
+
+describe("Returns session path", function(){
+  
+  it('Returns a 200 status code', function(done){
+    request(app)
+      .get('/sessions')
+      .expect(200, done)
   });
+  
+  it('Returns HTML format', function(done){
+    request(app)
+      .get('/sessions')
+      .expect('Content-Type', /html/, done)
+  });
+  
+  it('Returns the login view', function(done){
+    request(app)
+      .get('/sessions')
+      .expect(/sign in/i, done)
+  });
+});
