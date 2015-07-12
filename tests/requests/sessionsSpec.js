43f7e8409715f0ad868799efae22b295103f098e
Shaun Gordon <shaundev.gordon@gmail.com>
2015-07-12 12:54:24 +0100

    seperate spec files and moves into tests folder

--- tests/requests/sessionsSpec.js
+++ tests/requests/sessionsSpec.js
@@ -1,39 +1,20 @@
 var request = require('supertest');
-var app = require('./lib/server');
-
-
-describe('Requests Root path', function(){
-  
-  it('Returns a 200 status code', function(done){
-    
-  request(app)
-    .get('/')
-    .expect(200, done)
-    });
-    
-    it('Returns a HTML format', function(done) {
-
-      request(app)
-        .get('/')
-        .expect('Content-Type', /html/, done)
-    });
-    
-});
+var app = require('../../lib/server');
 
 describe("Returns session path", function(){
-  
+
   it('Returns a 200 status code', function(done){
     request(app)
       .get('/sessions')
       .expect(200, done)
   });
-  
+
   it('Returns HTML format', function(done){
     request(app)
       .get('/sessions')
       .expect('Content-Type', /html/, done)
   });
-  
+
   it('Returns the login view', function(done){
     request(app)
       .get('/sessions')
---
+++ tests/requests/test.js
@@ -0,0 +1,21 @@
+var request = require('supertest');
+var app = require('../../lib/server');
+
+
+describe('Requests Root path', function(){
+  
+  it('Returns a 200 status code', function(done){
+    
+  request(app)
+    .get('/')
+    .expect(200, done)
+    });
+    
+    it('Returns a HTML format', function(done) {
+
+      request(app)
+        .get('/')
+        .expect('Content-Type', /html/, done)
+    });
+    
+});
